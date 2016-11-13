d3434b49d9cd8a64182dfd0f730aa0a88b7675af
James Zhu <james.zhu.engineer@gmail.com>
2016-11-12 22:24:03 -0800

    Add tweet fetching

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,10 @@ var googlemaps = require('@google/maps')
 
 var router = express.Router();
 
-GOOGLE_API_KEY = "AIzaSyA7bEwV-NVZczh0NjDes7O1TnyXtPBFkwI"
+GOOGLE_API_KEY = "AIzaSyA7bEwV-NVZczh0NjDes7O1TnyXtPBFkwI";
+
+numTweets = 100;
+berkeleyGeocode = "37.8716,-122.2727,1000km";
 
 var googleMapsClient = googlemaps.createClient({ key: GOOGLE_API_KEY });
 
@@ -15,63 +18,59 @@ router.get('/search', function(req, res, next) {
   if (!hashtag) {
     res.status(400).send({error: "No query parameter 'hashtag' found."});
   } else {
-    tweets = fetchTweets(hashtag, 100, "37.8716,-122.2727,1000km");
-    fs.writeFile("./tweets.json", JSON.stringify(tweets.statuses), function(err) {
-        if(err) {
-          console.log(err);
-        } else {
-          console.log("tweets.json successfully saved.");
-        }
-    });
+    // getTweets
+    res.status(200).send({tweets: parsedTweets})
   }
 });
 
-// Extract the tweet text and geolocation coordinates.
-function extractTweetTextAndLocation(tweet) {
-  // Geocode an address. 
-  filteredTweet = {
-    text: tweet.text,
-    coordinates: tweet.coordinates
-  }
+function getTweets(hashtag, callback) {
+  var parsedTweets = [];
+    var tweets = fetchTweets(hashtag, numTweets, berkeleyGeocode, function(error, tweets, response) {
+      for (var i = 0; i < tweets.statuses.length; i++) {
+        tweet = tweets.statuses[i];
+        extractTweetTextLocation(tweet, function(extractedTweet) {
+          parsedTweets.push(extractedTweet);
+        });
+      };
+      // Write tweets to file.
+      fs.writeFile("./tweets.json", JSON.stringify(parsedTweets), function(err) {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log("tweets.json successfully saved.");
+          }
+      });
+    });
+    console.log("All tweets acquired.");
+    return callback(parsedTweets);
+}
 
+// Extract the tweet text and geolocation coordinates.
+function extractTweetTextLocation(tweet, callback) {
+  // Geocode an address. 
   if (!!tweet.coordinates) {
-    return Promise.resolve({
+    return callback({
       text: tweet.text,
-      coordinates: tweet.coordinates
+      coordinates: tweet.coordinates.coordinates.reverse()
     });
   } else {
     // If the tweet had no coordinates, geocode the human-readable user location.
-    return filteredTweet.coordinates = googleMapsClient.geocode({
-        address: tweet.user.location
-    }).asPromise()
-    .then(function(value) {
-      return Promise.resolve({
-        text: tweet.text, 
-        coordinates: value
-      })
-    })
-    .catch(function(reason) {
-      console.log("Tweet location not found (from tweet or user profile).");
-      console.dir(reason);
+    googleMapsClient.geocode({ address: tweet.user.location, components: "country:US"}, function(err, response) {
+      if (!err) {
+        var location = response.results[0].geometry.location;
+        var coords = [location.lat, location.lng];
+        callback({text: tweet.text, coordinates: coords})
+      }
     });
   }
 }
 
-function fetchTweets(hashtag, count, geocode) {
-  return client.get('search/tweets.json',
-    {
+function fetchTweets(hashtag, count, geocode, callback) {
+  return client.get('search/tweets.json', {
       q: hashtag,
       count: count,
       geocode: geocode
-    },
-    function(error, tweets, response) {
-      // Extract tweets and locations (lat & long) 
-      for (var i = 0; i < tweets.statuses.length; i++) {
-        tweet = tweets.statuses[i];
-        console.log(tweets.statuses[i].text);
-      };
-    }
-  );
+    }, callback);
 }
 
 module.exports = router;
